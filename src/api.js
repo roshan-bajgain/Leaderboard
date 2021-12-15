3021752db5933553fac53f068d4bd7830ad5940e
Roshan Bajgain <you@example.com>
2021-12-15 15:54:56 +0545

    Add function for add score

--- src/api.js
+++ src/api.js
@@ -1,9 +1,21 @@
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/wMP3RWMQEh4QdJl7qy8R/scores/';
 
-async function getScores (){
+async function getScores() {
     const response = await fetch(url);
     const data = await response.json();
     return data.result;
 }
 
+async function addScore(obj) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+});
+const data = await response.json();
+return data.result;
+}
 
+module.exports = { getScores, addScore };
