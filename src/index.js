f8c540bf5c2ebd9c9d233c419ce2767de9b1ea8c
Roshan Bajgain <you@example.com>
2021-12-15 19:25:46 +0545

    fix linter

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import { getScores, addScore } from './api.js';
 const btn = document.querySelector('.submit-btn');
 
 async function render() {
-    const array = await getScores();
-    table(array);
+  const array = await getScores();
+  table(array);
 }
 render();
 
 btn.addEventListener('click', async (e) => {
-    e.preventDefault();
-    const user = document.querySelector('#name').value;
-    const score = Number(document.querySelector('#score').value);
-    const obj = { user, score };
-    const response = await addScore(obj);
-    document.querySelector('#name').value = '';
-    document.querySelector('#score').value = '';
+  e.preventDefault();
+  const user = document.querySelector('#name').value;
+  const score = Number(document.querySelector('#score').value);
+  const obj = { user, score };
+  await addScore(obj);
+  document.querySelector('#name').value = '';
+  document.querySelector('#score').value = '';
 });
 
 document.querySelector('.refresh-button').addEventListener('click', () => render());
