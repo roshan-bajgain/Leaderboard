32e8226f3e5168daae1228d2a7cd3d67d2c47a3b
Roshan Bajgain <you@example.com>
2021-12-15 16:05:01 +0545

    Remove Hardcopies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+import { getScores, addScore } from './api.js';
+
+const btn = document.querySelector('.submit-btn');
+
+async function render() {
+    const array = await getScores();
+    table(array);
+}
+render();
+
+btn.addEventListener('click', async () => {
+    const user = document.querySelector('#name').value;
+    const score = Number(document.querySelector('#score').value);
+    const obj = { user, score };
+    addScore(obj);
+  document.querySelector('#name').value = '';
+  document.querySelector('#score').value = '';
+});
+
+document.querySelector('.btn-success').addEventListener('click', render);
